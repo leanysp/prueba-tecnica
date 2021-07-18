04b79b893b292e148560529612e922947f4861f7
Leanys <leanysp@gmail.com>
2021-07-18 16:39:09 -0500

    variable nombre en saludo rama hotfix

--- src/main/java/com/pineda/app/controllers/SaludoController.java
+++ src/main/java/com/pineda/app/controllers/SaludoController.java
@@ -5,6 +5,7 @@ package com.pineda.app.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SaludoController {
 	
-	@GetMapping("/")
-	public ResponseEntity<?> holaMundo(){
-		String saludo = "Hola a todos, bienvenidos";
+	@GetMapping("/{nombre}")
+	public ResponseEntity<?> holaMundo(@PathVariable String nombre){
+		String saludo = String.format("Hola a todos, mi nombre es %s, bienvenidos!", nombre);
 		return ResponseEntity.ok(saludo);
 	}
 
